762770e792b6559a1598d46f6971e19e2c5fdda4
Zombulelo <nombulelojk@outlook.com>
2017-05-07 19:22:46 +0200

    Add project files.

---
+++ FishLandApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace FishLandApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ FishLandApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FishLandApp.Global" Language="C#" %>
