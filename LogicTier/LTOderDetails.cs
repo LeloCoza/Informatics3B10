47d09493a90299a77c9755c96b343bea0ea04349
Zombulelo <nombulelojk@outlook.com>
2017-05-09 17:43:35 +0200

    created business logic methods for LTOderDetails class.

---
+++ LogicTier/LTOderDetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseLayer;    //import DatabaseLayer namespace
+
+namespace LogicTier
+{
+    public class LTOderDetails
+    {
+        public DLOderDetails objDataLayer = new DLOderDetails();  //create a DataLayer object
+
+        //create, select, update, delete functions to update from presentation layer to database layer
+        public void AddOderDetails(int oderDet_id, int size, int num_of_items)
+        {
+            objDataLayer.AddOderDetailsDB(oderDet_id, size, num_of_items);
+        }
+
+        public void UpdateOderDetails(int oderDet_id, int size, int num_of_items)
+        {
+            objDataLayer.UpdateOderDetailsDB(oderDet_id, size, num_of_items);
+        }
+
+        public void DeleteOderDetails(int oderDet_id)
+        {
+            objDataLayer.DeleteOderDetailsDB(oderDet_id);
+        }
+
+        public object RetrieveOderDetails()
+        {
+            return objDataLayer.RetrieveOderDetailsDB();
+        }
+    }
+}
