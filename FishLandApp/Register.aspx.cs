7025b2b82ffd9e81d7650522fc787348b45deea1
Zombulelo <nombulelojk@outlook.com>
2017-05-09 15:05:27 +0200

    imported namespace LogicTier

--- FishLandApp/Register.aspx.cs
+++ FishLandApp/Register.aspx.cs
@@ -5,21 +5,21 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-using BusinessLayer;
+using LogicTier;
 
 namespace FishLandApp
 {
     public partial class Register : System.Web.UI.Page
     {
-        BLActor objLogic;
+        LTActor objLogic;
         protected void Page_Load(object sender, EventArgs e)
         {
-            objLogic = new BLActor();
+            objLogic = new LTActor();
         }
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
-            objLogic = new BLActor();
+            objLogic = new LTActor();
 
             objLogic.AddActor(txtName.Text, txtSurname.Text, txtDOB.Text, txtCellphone.Text, txtEmail.Text, txtAddress.Text);
            // Response.Write("You have been registered!");
