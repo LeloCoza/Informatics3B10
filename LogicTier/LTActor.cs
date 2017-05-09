da45d4a461dbe5a94781f82dc0b9fe8cac91424a
Zombulelo <nombulelojk@outlook.com>
2017-05-09 16:38:44 +0200

    deleted DLActorIdentity and LTActorIdentity

--- LogicTier/LTActor.cs
+++ LogicTier/LTActor.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using DatabaseLayer;
+using DatabaseLayer;    //import DatabaseLayer
 
 namespace LogicTier
 {
--- LogicTier/LTAuthentication.cs
+++ LogicTier/LTAuthentication.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DatabaseLayer;    //import DatabaseLayer
+
 namespace LogicTier
 {
     class LTAuthentication
     {
+        public DLAuthentication objDataLayer = new DLAuthentication();  //create a DataLayer object
+
+        //create, select, update, delete functions to update from presentation layer to database layer
+        public void AddAuthentication(string uname, string password, string cpassword)
+        {
+            objDataLayer.AddAuthenticationDB(uname, password, cpassword);
+        }
+
+        public void UpdateAuthentication(int authentication_id, string uname, string password, string cpassword)
+        {
+            objDataLayer.UpdateAuthenticationDB(authentication_id, uname, password, cpassword);
+        }
+
+        public void DeleteAuthentication(int authentication_id)
+        {
+            objDataLayer.DeleteAuthenticationDB(authentication_id);
+        }
+
+        public object RetrieveAuthentication()
+        {
+            return objDataLayer.RetrieveAuthenticationDB();
+        }
     }
 }
