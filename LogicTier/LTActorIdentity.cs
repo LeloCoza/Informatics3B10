c7daf70972c56768efc618306ee3a25a25f9c975
Zombulelo <nombulelojk@outlook.com>
2017-05-09 16:08:46 +0200

    fixed unnecessary errors.

--- LogicTier/LTActorIdentity.cs
+++ LogicTier/LTActorIdentity.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DatabaseLayer;
+ 
+using DatabaseLayer;    //import DatabaseLayer namespace
 
 namespace LogicTier
 {
