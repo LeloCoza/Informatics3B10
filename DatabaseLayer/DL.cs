bd9d1b2f23155fcf53269e04fe0f3dfb263d331d
Zombulelo <nombulelojk@outlook.com>
2017-05-09 14:49:21 +0200

    created DatabaseLayer namespace and ...

---
+++ DatabaseLayer/DL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;     //import data references
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatabaseLayer
+{
+    public class DL
+    {
+    }
+}
