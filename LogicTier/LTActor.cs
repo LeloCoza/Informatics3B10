bb5681a59d57b3def7071ad6d430e1fefa653151
Zombulelo <nombulelojk@outlook.com>
2017-05-09 15:00:57 +0200

    coded methods for both DLActor and LTActor classes

--- LogicTier/LTActor.cs
+++ LogicTier/LTActor.cs
@@ -10,5 +10,27 @@ namespace LogicTier
 {
     public class LTActor
     {
+        public DLActor objDataLayer = new DLActor();  //create a DataLayer object
+
+        //create, select, update, delete functions to update from presentation layer to database layer
+        public void AddActor(string actor_name, string actor_surname, string actor_DOB, string actor_cell, string actor_email, string actor_address)
+        {
+            objDataLayer.AddActorDB(actor_name, actor_surname, actor_DOB, actor_cell, actor_email, actor_address);
+        }
+
+        public void UpdateActor(int actor_id, string actor_name, string actor_surname, string actor_DOB, string actor_cell, string actor_email, string actor_address)
+        {
+            objDataLayer.UpdateActorDB(actor_id, actor_name, actor_surname, actor_DOB, actor_cell, actor_email, actor_address);
+        }
+
+        public void DeleteActor(int actor_id)
+        {
+            objDataLayer.DeleteActorDB(actor_id);
+        }
+
+        public object RetrieveActor()
+        {
+            return objDataLayer.RetrieveActorDB();
+        }
     }
 }
