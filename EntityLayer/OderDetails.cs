1050fd90a478fa38226b4871e0c75b8886c53d35
Zombulelo <nombulelojk@outlook.com>
2017-05-25 15:07:53 +0200

    defined an entity class for OderDetails table

---
+++ EntityLayer/OderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class OderDetails
+    {
+        public int od_id { get; set; }
+        public int oder_id { get; set; }
+        public int prod_id { get; set; }
+        public int size { get; set; }
+        public int num_of_items { get; set; }
+    }
+}
