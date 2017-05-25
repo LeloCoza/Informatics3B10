03348e4dabf72222d20545e1f0ea7d77dbad595a
Zombulelo <nombulelojk@outlook.com>
2017-05-25 15:34:28 +0200

    added an entity class for Product table in the EntityLayer namespace

---
+++ EntityLayer/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Product
+    {
+        public int prod_id { get; set; }
+        public string prod_name { get; set; }
+        public string prod_descript { get; set; }
+        public string prod_price { get; set; }
+        public string prod_pic { get; set; }
+        public int cat_id { get; set; }
+    }
+}
